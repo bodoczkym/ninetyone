cc6a5acbfefeee83a6d2520562104c367dab9c3c
Szandi <szalai.sandee@gmail.com>
2019-03-08 12:21:38 +0100

    Add 'Note'

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ insert into user(username, password, enabled, role, reference, description, rate
 insert into user(username, password, enabled, role, reference, description, rate, img, email) values (
   'user1', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_USER', 'https://www.youtube.com/watch?v=YFXlzKDjL8E', 'He is sooo funnyHe is sooo funnyHe is sooo funny', 2, '/assets/images/img3.jpg', 'asd@gmail.com');
 insert into user(username, password, enabled, role, reference, description, rate, img, email) values (
-  'owner3', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_OWNER', 'https://www.youtube.com/watch?v=YFXlzKDjL8E', 'He is sooo funnyjnfkj nfkjas gnjkasb gkadbgdkafhbghd fabghl fabgdjfbjad fljgkrskaljgbrjks ahlkjabgkl fdjagblkfdjabgl kdfajbgldkfagbl fdabl hdafbgkhbaed lghbrwlgbl krejabgraebgb.', 3, '/assets/images/imggith1.png', 'asd@gmail.com');
+  'owner3', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_OWNER', 'https://www.youtube.com/watch?v=YFXlzKDjL8E', 'He is sooo funnyjnfkj nfkjas gnjkasb gkadbgdk afhbghd fabghl fabgdjfbjad fljgkrsk aljgbrjks ahlkjabgkl fdjagbl fdjabgl kdfaj bgldkfagbl fdabl hdafbg khbaed lghbrwl bl krejabgraebgb.', 3, '/assets/images/img1.png', 'asd@gmail.com');
 insert into user(username, password, enabled, role, reference, description, rate, img, email) values (
   'user2', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_USER', 'https://www.youtube.com/watch?v=YFXlzKDjL8E', 'He is sooo funnyjnfkj nfkjas gnjkasb gkadbgdkafhbghd fabghl fabgdjfbjad fljgkrskaljgbrjks ahlkjabgkl fdjagblkfdjabgl kdfajbgldkfagbl fdabl hdafbgkhbaed gkadbgdkafhbghd flgbl krejabgraebgb.', 4, '/assets/images/img3.jpg', 'asd@gmail.com');
 insert into user(username, password, enabled, role, reference, description, rate, img, email) values (
@@ -42,3 +42,9 @@ insert into purchase(user_id, product_id, order_name,address,phone_number,is_del
   (1, 3, 'prder2','proba2','230312342',false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 insert into purchase(user_id, product_id, order_name,address,phone_number,is_delivered, created_at, updated_at) values
   (6, 1, 'prder3','proba3','330312342',true, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+insert into notes ( name, text ) values ('Karcsi', 'I hope it will be a wonderful page');
+insert into notes ( name, text ) values ('Bela', 'Do the TODOs');
+insert into notes ( name, text ) values ('asf', 'I hope it will be a wonderful pagehope it will be a wonderful page hope it will be a wonderful page hope it will be a wonderful pagehope it will be a wonderful page');
+insert into notes ( name, text ) values ('ee', 'fffffffffffffffaasdasdafafadfdfsdfsdfffffffffffffffaasdasdafafadfdfsdfsdfffffffffffffffaasdasdafafadfdfsdfsdfffffffffffffffaasdasdafafadfdfsdfsdfffffffffffffffaasdasdafafadfdfsdfsdfffffffffffffffaasdasdafafadfdfsdfsdfffffffffffffffaasdasdafafadfdfsdfsdfffffffffffffffaasdasdafafadfdfsdfsdfffffffffff');
+insert into notes ( name, text ) values ('zozu', 'I hope it will be a wonderful page');
